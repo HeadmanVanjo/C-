bb06297f301cced7b7add0f7612b11dfbbcbfcc7
headman <vahtsyan@gmail.com>
2020-03-19 10:42:11 +0100

    My C# work

---
+++ C#/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vahts
+ * Date: 3/17/2020
+ * Time: 10:44 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace C_
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{ 
+			
+			}
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
