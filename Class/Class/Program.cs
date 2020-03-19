fe5bed6b5ba2a698b148a656cdb4f044d40aaf66
headman <vahtsyan@gmail.com>
2020-03-19 13:09:08 +0100

    dope

---
+++ Class/Class/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vahts
+ * Date: 3/19/2020
+ * Time: 10:49 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Class
+
+{
+	public static class addition{
+		public static double sum (double a){
+			return a + 68;
+	}
+	
+    	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			Console.WriteLine(addition.sum(45));
+//			Console.WriteLine(I.love(6));
+//			Console.WriteLine(Me.Headman(9));
+//			We.Won();
+//			Work.Done();
+//			Mine.life();
+//			Am.Dope();
+//			Console.Write("Enter A Number: ");
+//			int a = Convert.ToInt32(Console.ReadLine());
+//			
+//			Console.Write("Enter A Number: ");
+//			
+//			int b = Convert.ToInt32(Console.ReadLine());
+//
+//			int c = a + b;
+//			Console.WriteLine(c);
+			
+//			int a = 2;
+//			int b = 5;
+//			string Headman;
+//			Headman = a>b? "true":"false";
+//			Console.WriteLine(Headman);
+				
+			// TODO: Implement Functionality Here
+			 
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			
+		}
+
+		}
+	}}
+
